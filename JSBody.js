4fce5779fc2c3e16eee38336c66e2fc24cbc9346
AaronReviews <67519708+AaronReviews@users.noreply.github.com>
2022-05-28 17:05:56 +0100

    Add files via upload

---
+++ JSBody.js
@@ -0,0 +1,16 @@
+﻿$.getJSON('../lib/PageInfo.json', function(Json) {
+    counter = 0;
+    review = Json.reviews;
+    content = Json.content;
+    
+    $.each(content, function(index, value) {
+        create_content(value);
+    });
+    
+    $.each($('.Mcontainer').get(), function() {
+        $("<div class='boxM'></div>").insertBefore($(this));
+        $('.boxM').last().append("<blockquote class='review' align='center'></blockquote>");
+        $('.review').last().append(review[counter]['comment']);
+        counter += 1;
+    });
+});
--- JSHead.js
+++ JSHead.js
@@ -1,25 +1,27 @@
-﻿// var xmlhttp;
-// var pull;
+﻿function create_content(content) {
+    $("<div class='Mcontainer' align='center'></div>").insertBefore("#placeholder");
+    $.each(content, function (index, value) {
+        $('.Mcontainer').last().append("<p class='text'></p>")
+        $('.text').last().append(value);
+    });
+}
 
-// if (window.XMLHttpRequest) {
-//     xmlhttp = new XMLHttpRequest();
-// } else {
-//     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-// }
-
-// try {
-//     xmlhttp.open("GET", "lib/Reviews.xml", true);    //configures the connection - async set to true
-//     xmlhttp.send();                                  //sends out the command to open the connection
-//     xmlhttp.onreadystatechange = function() {
-//         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//             xmlhttp.onload = Rotate(xmlhttp); pull = true; }
-//     };
-// }
-// catch(err) { pull = false; }
-
-$(document).ready(function() {
-    $.getJSON('PageInfo.json', function(Json) {
-        var content = Json.content;
-        var colours = Json.colour;
-    });              
- });
+$.getJSON('../lib/PageInfo.json', function(Json) {
+    var colours = Json.colour['default'];           
+    $(':root').css({
+        '--color0': colours[0],
+        '--color1': colours[1],
+        '--color2': colours[2],
+        '--color3': colours[3],
+        '--color4': colours[4],
+        '--color5': colours[5],
+        '--color6': colours[6],
+        '--color7': colours[7],
+        '--T': 1.1 + 'em',
+        '--M': function() {
+            if (screen.width <= screen.height) { width = screen.width / 8 + 'px'; } 
+            else { width = screen.height / 8 + 'px'; }
+            return width;
+        }
+    })
+});
