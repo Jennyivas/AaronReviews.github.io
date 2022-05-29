6a0e5317cc2cb59220fa4cb60a7aabce42ddcbd5
AaronReviews <67519708+AaronReviews@users.noreply.github.com>
2022-05-30 00:10:40 +0100

    Update JSHead.js

--- JSHead.js
+++ JSHead.js
@@ -1,8 +1,9 @@
-var page = document.getElementsByName("PageMarker");
+var page = document.getElementById("PageMarker");
 var Id = page.dataset['pageId'];
 var link_path;
 if (Id == 'Mobile') { link_path = 'PageInfo.json'; } 
 else { link_path = 'lib/PageInfo.json'; }
+console.log(page.dataset)
 console.log(Id);
 console.log(link_path);
 
